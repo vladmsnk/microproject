3d1cd4e7fd0a37f123dddfe278916959113ac13b
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-03-23 23:42:09 +0300

    upload json files created the zulip array messages of mine created the array of dates

--- micro/first.py
+++ micro/first.py
@@ -1,2 +1,21 @@
 import json
-print("Hello World")
+import pandas as pd
+with open('ZulipStats.json') as zulip:
+    data1 = json.load(zulip)
+with open('GitStats.json') as Git:
+    data2 = json.load(Git)
+with open('JitsiClasses.json') as JitsiClasses:
+    data3 = json.load(JitsiClasses)
+with open('JitsiSession.json') as JitsiSession:
+    data4 = json.load(JitsiSession)
+
+
+
+dfzulip = pd.DataFrame(data1)
+
+my_data = dfzulip[dfzulip['email'] =='vyumoiseenkov@miem.hse.ru']
+zulip_messages = my_data.iloc[0]['messages'] #type - dict
+timez =[]
+for i in range(len(zulip_messages)):
+    timez.append(zulip_messages[i]['timestamp'])
+print(timez)

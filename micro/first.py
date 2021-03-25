037ac7e22e97ac3eda3e8aee5485f12ab113c4d2
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-03-25 13:30:28 +0300

    made split array

--- micro/first.py
+++ micro/first.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from datetime import datetime
 import numpy as np
-import pprint
+import math
 
 import matplotlib.pyplot as plt
 
@@ -18,6 +18,12 @@ with open('JitsiClasses.json') as JitsiClasses:
 with open('JitsiSession.json') as JitsiSession:
     data4 = json.load(JitsiSession)
 
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def divide_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
 # zulip
 dfzulip = pd.DataFrame(data1)
 my_data = dfzulip[dfzulip['email'] =='vyumoiseenkov@miem.hse.ru']
@@ -32,24 +38,13 @@ for i in range(len(zulip_messages)):
 
 new_array = list(map(lambda x: x.date().month,timezulip))
 
-# timezulip.append('2021-01-17T22:44:45')
-# timegit = []
-# for j in range(len()):
-# a= '2021-01-17T22:44:45'
-# a = a.replace('T',' ')
-# timezulip.append(datetime.strptime(a,'%Y-%m-%d %H:%M:%S'))
-# print(timezulip)
-# print(zulip_messages)
-# print(timezulip[0].date())
-# print(new_array)
-
 c = Counter()
 for word in new_array:
     c[word]+=1
 x = list(c.keys())
 y =  list(c.values())
 labels = ['January','February','March']
-range = np.arange(1,4,1)
+rang = np.arange(1,4,1)
 
 # jitsi
 dfjitsi = pd.DataFrame(data4)
@@ -58,28 +53,19 @@ begin = np.array(list(map(lambda x: int(x[:5].replace(':','')),mydate['begin']))
 end = np.array(list(map(lambda x: int(x[:5].replace(':','')),mydate['end'])))
 for_sec_array = end - begin
 for_sec_array = for_sec_array[for_sec_array > 40].tolist()
-print(type(for_sec_array))
-# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
-# print(len(for_sec_array) / 3)
-# yjits = [len(for_sec_array[:4]),len(for_sec_array[4:8]),len(for_sec_array[8:])]
-# def chunks(lst, n):
-#     a = [lst[i:i + n] for i in range(0, len(lst), n)]
-#     return a
-a = [1,23,4,5,6,8,9,3,1,4,5,14,5,23,4,5,1,4]
-def splt(lst,n):
-    yield [lst[i:i + n] for i in range(0, len(lst), n)]
-def splt(l, n):
-    n = max(1, n)
-    return (l[i:i+n] for i in range(0, len(l), n))
-print(list(splt(a,3)))
 
-# pprint.pprint(list(chunks(for_sec_array,3)))
+new2 = list(divide_chunks(list(for_sec_array),math.ceil(len(for_sec_array)/3)))
+
+# b = list(divide_chunks(my_list, 4))
+# print (b)
+# yjits = list(divide_chunks(my_list, math.ceil(len(for_sec_array)/3)))
+# print(yjits)
 # plt.scatter(x,y,color = 'r',label = 'Zulip')
 # plt.legend()
 # plt.xlabel('Date')
 # plt.xticks(np.arange(1,5,1))
 # plt.ylim(0,10)
 # plt.grid()
-# plt.xticks(range,labels = labels)
+# plt.xticks(rang,labels = labels)
 # plt.ylabel('activity Count')
 # plt.show()
